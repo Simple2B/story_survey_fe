dc4d97cd67ea632df08069025ebcf7be3d2377d4
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-06-13 17:33:10 +0300

    gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1ac2faa8645c736"; }
+/******/ 		__webpack_require__.h = function() { return "e28602c12f52d3fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
