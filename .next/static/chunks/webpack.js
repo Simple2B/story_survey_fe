4e39c26a775802d2a2ab56ee247c724ac58260be
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-06-13 17:10:09 +0300

    add packege

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fcb936bebb5efe15"; }
+/******/ 		__webpack_require__.h = function() { return "f1ac2faa8645c736"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
