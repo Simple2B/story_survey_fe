0aaadfb0e122a7ea412decd604ec85ea35af370a
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-06-14 09:41:46 +0300

    gitignore

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import '../styles/main.css'
+import { SessionProvider } from "next-auth/react"
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
