ee7bfd7fb5db421c301f0dc75de1f34aca58c7ac
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-06-14 11:20:48 +0300

    types

---
+++ redux/types/authTypes.ts
@@ -0,0 +1,56 @@
+// state
+export interface IAuthState {
+    loggedIn: boolean;
+    errorMessage: string;
+    isLoading: boolean;
+  }
+  // action types
+  export enum AuthActionTypes {
+    AUTH_API_REQUEST = "AUTH_API_REQUEST",
+    LOGIN_SUCCESS = "LOGIN_SUCCESS",
+    LOGIN_FAILURE = "LOGIN_FAILURE",
+    LOGOUT = "LOGOUT",
+    CHECK_TOKEN = "CHECK_TOKEN",
+  }
+  
+  interface IAuthApiRequestAction {
+    type: AuthActionTypes.AUTH_API_REQUEST;
+  }
+  interface ILoginRequestSuccessAction {
+    type: AuthActionTypes.LOGIN_SUCCESS;
+    payload: ILoginResponse;
+  }
+  interface ILoginRequestFailureAction {
+    type: AuthActionTypes.LOGIN_FAILURE;
+    payload: string;
+  }
+  interface ILogoutAction {
+    type: AuthActionTypes.LOGOUT;
+  }
+  interface ICheckToken {
+    type: AuthActionTypes.CHECK_TOKEN;
+    payload: ILoginResponse;
+  }
+  
+  export type AuthAction =
+    | IAuthApiRequestAction
+    | ILoginRequestSuccessAction
+    | ILoginRequestFailureAction
+    | ILogoutAction
+    | ICheckToken;
+  
+  // object types
+  export interface ILoginResponse {
+    access_token: string;
+    token_type: string;
+  }
+  
+  export interface ILogoutResponse {
+    status: string;
+    detail: string;
+  }
+  export interface ILoginParams {
+    username: string;
+    password: string;
+  }
+  
