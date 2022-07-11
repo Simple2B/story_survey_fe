d63843bbd650fe060d20b0b4050e51bb93b66950
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-11 13:11:02 +0300

    table style

--- components/UserProfile/SurveyList/TableSurveyList.tsx
+++ components/UserProfile/SurveyList/TableSurveyList.tsx
@@ -11,42 +11,40 @@ const TableSurveyList = ({userSurveys, setUserSurveys, copyLink, link}): ReactEl
     const [isCopied, setCopied] = useState<boolean>(false);
     
     return  (
-        <div className={styles.container}>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Question</th>
-                    <th scope="col">Link</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {userSurveys && userSurveys.length > 0 && (
-                        userSurveys.map((survey, index) => {
-                            
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{index+1}</th>
-                                    <td>{survey.title}</td>
-                                    <td>{survey.description}</td>
-                                    <td>{survey.questions.length}</td>
-                                    <td className={styles.linkRow} onClick={() => copyLink(survey.id, survey.title)}>
-                                        <span className={styles.copyLinkTitle}>copy link</span>
-                                        <span className={styles.copyLink}>
-                                            {`${link}/survey/${survey.id}`}
-                                        </span>
-                                    </td>
-                                </tr>
-                            )
-                        })
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Question</th>
+                <th scope="col">Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                {userSurveys && userSurveys.length > 0 && (
+                    userSurveys.map((survey, index) => {
                         
-                    )} 
+                        return (
+                            <tr key={index}>
+                                <th scope="row">{index+1}</th>
+                                <td>{survey.title}</td>
+                                <td>{survey.description}</td>
+                                <td>{survey.questions.length}</td>
+                                <td className={styles.linkRow} onClick={() => copyLink(survey.id, survey.title)}>
+                                    <span className={styles.copyLinkTitle}>copy link</span>
+                                    <span className={styles.copyLink}>
+                                        {`${link}/survey/${survey.id}`}
+                                    </span>
+                                </td>
+                            </tr>
+                        )
+                    })
                     
-                </tbody>
-            </table>
-        </div>
+                )} 
+                
+            </tbody>
+        </table>
     );
 };
 
