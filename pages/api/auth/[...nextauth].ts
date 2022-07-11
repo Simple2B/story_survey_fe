bd0aa12a66eb7af7254b55503423901c14af30a8
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-11 10:39:13 +0300

    next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,26 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { clientApi } from "../backend/userInstance";
+// import NodeCache from 'node-cache';
+
+// function EmailTokenNodeCacheAdapter(client, options) {
+//   return {
+//     getUserByEmail: async (email) => ({ id: email, email }),
+
+//     getUserByAccount: async ({ providerAccountId, provider }) => ({ id: providerAccountId, provider }),
+
+//     updateUser: async (user) => user,
+
+//     createVerificationToken: async (data) => {
+//       return client.set(data.identifier, data) ? data : null
+//     },
+
+//     useVerificationToken: async ({ identifier, token }) => {
+//       const data = client.get(identifier);
+//       return (data?.token === token) ? client.take(identifier) : false;
+//     }
+//   }
+// };
 
 
 export default NextAuth({
@@ -32,6 +52,8 @@ export default NextAuth({
   // theme: {
   //   colorScheme: "light",
   // },
+  // adapter: EmailTokenNodeCacheAdapter(new NodeCache({ stdTTL: 24 * 60 * 60, checkperiod: 1 * 60 * 60 }), {}),
+
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       
