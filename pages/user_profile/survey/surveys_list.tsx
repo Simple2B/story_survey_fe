dd00736ca3b78e8b1dbff3457dceb1c548ca0af5
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-11 10:39:05 +0300

    survey list

--- pages/user_profile/survey/surveys_list.tsx
+++ pages/user_profile/survey/surveys_list.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import ToggleSwitch from "../../../components/common/ToggleSwitchBtn/ToggleSwitchBtn";
 import UserContainer from "../../../components/Containers/MainContainer/UserContainer";
 import SurveyList from "../../../components/UserProfile/SurveyList/SurveyList";
 import User from "../../../components/UserProfile/User";
 
 
 const ProfileSurveyLists = ({headerName}) => {
-  // const { data: session } = useSession();
+  const [checked, setChecked] = useState(false);
+  
+  const handleChangeChecked = () => {
+    setChecked(!checked);
+  }
+
   return (
     <User title={'Survey List'} keywords={""} style={""} headerName={'Survey List'}>
-      <SurveyList />
+      <div className="surveyListContainer">
+        <ToggleSwitch checked={checked} onChange={handleChangeChecked} id={"SurveyList"}/>
+        {
+          !checked ? 
+            <SurveyList />
+            :
+            <div>Table</div>
+        }
+        
+        
+      </div>
     </User>
   )
 }
