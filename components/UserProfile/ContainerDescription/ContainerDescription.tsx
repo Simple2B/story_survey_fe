ee4a2e9bdba49c46d4c4a38b921de2ff197adb25
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-21 14:26:06 +0300

    description

---
+++ components/UserProfile/ContainerDescription/ContainerDescription.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react';
+import Image from "next/image";
+import styles from "./ContainerDescription.module.css";
+import deleteIcon from "../../../styles/icons/icons8-cancel-64.png";
+
+const ContainerDescription = ({setOpenDescription, isOpenDescription, survey, openQuestion}): ReactElement => {
+
+    return (
+        <div className={styles.modalWindow}>
+            <div className={styles.modal}>
+                <i className={styles.editIcon} onClick={() => setOpenDescription(!isOpenDescription)}>
+                    <Image src={deleteIcon} height={30} width={30}/>
+                </i>
+                <div className={styles.titleShowMore}>
+                    {survey.title}
+                </div>
+                <div className={styles.description}>
+                    {survey.description}
+                </div>
+                <button 
+                    className={`${styles.btn} ${styles.btnPrimary} ${styles.btnBlock}`} 
+                    onClick={() => openQuestion(survey)}
+                >
+                        Answer the {survey.questions.length > 0 ? "questions" : "question"}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ContainerDescription;
